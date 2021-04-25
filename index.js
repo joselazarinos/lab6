48513d158c1f7c17d96d1ddd832af72fde7472c5
jose fernando <jose.lazarinos@tecsup.edu.pe>
2021-04-24 23:36:40 -0500

    info

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const http = require('http')
-
-const alumno = [
-    {id: '1', nombre: 'Luis', numero: '987654321'},
-    {id: '2',nombre: 'Juan', numero: '987654322'},
-    {id: '3',nombre: 'papaluchon', numero: '987654323'},
-    {id: '4',nombre: 'Angel', numero: '987654324'}
+const express = require('express')
+const app = express()
+let alumno = [
+    {id: '1', nombre: 'Fernandez', numero: '78458482159'},
+    {id: '2',nombre: 'Pedro', numero: '48579856485315'},
+    {id: '3',nombre: 'Anthonio', numero: '5899486523'},
+    {id: '4',nombre: 'Jhoshi', numero: '47851285632'}
 ]
-
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    response.end(JSON.stringify(alumno))
+app.get('/', (request, response) => {
+    response.json(alumno)
 })
 
+app.get('/info', (request, response) => {
+    const cantidad = alumno.length
+    response.send('<h1>Hay ' + cantidad + ' personas en la agenda</h1>')
+})
+
+
+app.get('/info/:id', (request, response) => {
+    const id = request.params.id
+    const note = alumno.find(alumno => alumno.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
 const PORT = 3001
-app.listen(PORT)
-console.log('listening on port ' + PORT)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
