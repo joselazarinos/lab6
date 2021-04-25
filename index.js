a92b84f03d4fc70c3e4ccb380ce882238f6601ac
jose fernando <jose.lazarinos@tecsup.edu.pe>
2021-04-24 23:41:00 -0500

    info

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ let alumno = [
     {id: '1', nombre: 'Fernandez', numero: '78458482159'},
     {id: '2',nombre: 'Pedro', numero: '48579856485315'},
     {id: '3',nombre: 'Anthonio', numero: '5899486523'},
-    {id: '4',nombre: 'Jhoshi', numero: '47851285632'}
+    {id: '4',nombre: 'Jhoshi', numero: '47851285632'},
+    {id: '5',nombre: 'Homercules', numero: '5558496553'},
+    {id: '6',nombre: 'Morshi', numero: '99854889561523'}
 ]
 app.get('/', (request, response) => {
     response.json(alumno)
